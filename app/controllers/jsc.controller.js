85fa0328f1f1a9444e8f0aa69aeceec01eb24ad9
sanseru <sanseru1@yahoo.com>
2021-01-26 15:28:28 +0700

    Perbaikan

--- app/controllers/jsc.controller.js
+++ app/controllers/jsc.controller.js
@@ -1,25 +1,19 @@
 const Customer = require("../models/jsc.model.js");
 
-// Create and Save a new Customer
-exports.create = (req, res) => {
-  
-};
-
-// Retrieve all Customers from the database.
+// Retrieve all.
 exports.findAll = (req, res) => {
-    Customer.getAll((err, data) => {
-        if (err)
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while retrieving customers."
-          });
-
-        else 
-        rspon = {
-          status : "success",
-          count  : "1",
-          data  : [data]
-        }
-        res.send(rspon);
+  Customer.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving customers."
       });
+
+    else
+      rspon = {
+        status: "success",
+        count: "1",
+        data: [data]
+      }
+    res.send(rspon);
+  });
 };
--- app/models/jsc.model.js
+++ app/models/jsc.model.js
@@ -1,77 +1,68 @@
 // const sql = require("./db.js");
 const axios = require("axios");
-const request = require('postman-request')
 
-const Jsc = function(jsc) {
-    this.key = jsc.key;
-  };
-
+const Jsc = function (jsc) {
+  this.key = jsc.key;
+};
 
 
 
-  Jsc.getAll = result => {
-    // Optionally the request above could also be done as
-      axios.get('https://api.jakarta.go.id/v1/rumahsakitumum', {
+
+Jsc.getAll = result => {
+  // Optionally the request above could also be done as
+  axios.get('https://api.jakarta.go.id/v1/rumahsakitumum', {
+      headers: {
+        Authorization: 'LdT23Q9rv8g9bVf8v/fQYsyIcuD14svaYL6Bi8f9uGhLBVlHA3ybTFjjqe+cQo8k'
+      }
+    })
+    .then(response => {
+      this.data = response.data.data[0];
+
+      axios.get('https://api.jakarta.go.id/v1/kelurahan/' + this.data.kode_kelurahan, {
         headers: {
           Authorization: 'LdT23Q9rv8g9bVf8v/fQYsyIcuD14svaYL6Bi8f9uGhLBVlHA3ybTFjjqe+cQo8k'
         }
-      })
-      .then(response =>  {
-        // console.log(response.data.data)
-        this.data = response.data.data[0];
-        // console.log(this.data)
+      }).then(response => {
+        datax = {
+          id: this.data.id,
+          nama_rsu: this.data.nama_rsu,
+          jenis_rsu: this.data.jenis_rsu,
+          location: this.data.location,
+          kode_pos: this.data.kode_pos,
+          telepon: this.data.telepon,
+          faximile: this.data.faximile,
+          website: this.data.website,
+          email: this.data.email,
+          kelurahan: {
+            kode: response.data.data[0].kode_kelurahan,
+            nama: response.data.data[0].nama_kelurahan,
 
-        // this.data.forEach((item) => {
-        //   console.log("found: ", item)
-        //   result(null, item);
-        //   return;
-        // });
-        axios.get('https://api.jakarta.go.id/v1/kelurahan/'+this.data.kode_kelurahan, {
-          headers: {
-            Authorization: 'LdT23Q9rv8g9bVf8v/fQYsyIcuD14svaYL6Bi8f9uGhLBVlHA3ybTFjjqe+cQo8k'
+          },
+          kecamatan: {
+            kode: response.data.data[0].kode_kecamatan,
+            nama: response.data.data[0].nama_kecamatan,
+          },
+          kota: {
+            kode: response.data.data[0].kode_kota,
+            nama: response.data.data[0].nama_kota,
           }
-        }) .then(response =>  {
-          // console.log(response.data.data[0].nama_kelurahan)
-          datax = {
-            id: this.data.id,
-            nama_rsu: this.data.nama_rsu,
-            jenis_rsu: this.data.jenis_rsu,
-            location: this.data.location,
-            kode_pos: this.data.kode_pos,
-            telepon: this.data.telepon,
-            faximile: this.data.faximile,
-            website: this.data.website,
-            email: this.data.email,
-            kelurahan : {
-               kode : response.data.data[0].kode_kelurahan,
-               nama : response.data.data[0].nama_kelurahan,
-               
-            },
-            kecamatan : {
-              kode : response.data.data[0].kode_kecamatan,
-              nama : response.data.data[0].nama_kecamatan,
-           },
-           kota : {
-            kode : response.data.data[0].kode_kota,
-            nama : response.data.data[0].nama_kota,
-         }
 
-          }
-          result(null, datax  );
-          return;
-        });
-
-
-      })
-      .catch(function (error) {
-        console.log(error);
-        result(null, error);
+        }
+        result(null, datax);
         return;
-
-      })
-
-  };
+      });
 
 
- 
-  module.exports = Jsc;
+    })
+    .catch(function (error) {
+      console.log(error);
+      result(null, error);
+      return;
+
+    })
+
+};
+
+
+
+module.exports = Jsc;
